94f73332f693e62b51e1ba0bb0c538796eea40ab
Jorge FdCD <jfdecd@icloud.com>
2024-03-25 13:58:56 +0100

    devcontainer

---
+++ .devcontainer/onCreate.jl
@@ -0,0 +1,12 @@
+import Pkg
+
+Pkg.Registry.add(Pkg.RegistrySpec(url="https://github.com/cossio/CossioJuliaRegistry.git"))
+Pkg.Registry.add("General")
+
+# install some packages on global env
+Pkg.activate()
+Pkg.add([
+    "MyRegistrator",
+    "Revise",
+    "LocalRegistry",
+])
